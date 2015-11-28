2ce62994420c7848c8dd8a0752cca265e5267270
Jose Antonio Martin H <joseantonio.martinhernandez@telefonica.com>
2015-11-28 23:42:30 +0100

    Adding the 2nd set of functionality and many more widgets

---
+++ orangecontrib/spark/tutorials/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Jose Antonio Martin H."
+__copyright__ = "Copyright 2015, Jose Antonio Martin H."
+__credits__ = ["The Orange Machine Learning Project, Jose Antonio Martin H. "]
+__license__ = "Apache License 2.0"
+__maintainer__ = "JOse Antonio Martin H."
+__email__ = "xjamartinh@gmail.com"
