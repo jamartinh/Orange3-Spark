fd903c2dc2af1f3f045c1f9e5e4c5d842375e673
Jose Antonio Martin H <joseantonio.martinhernandez@telefonica.com>
2015-12-18 14:12:11 +0100

    bump to version 0.2.2

--- orangecontrib/spark/widgets/data/spark_fill.py
+++ orangecontrib/spark/widgets/data/spark_fill.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 import pyspark
 from Orange.widgets import widget, gui, settings
+from Orange.widgets.settings import Setting
 from PyQt4 import QtGui
 
 from orangecontrib.spark.utils.gui_utils import GuiParam
@@ -22,6 +23,7 @@ class OWSparkFillNa(widget.OWWidget):
     in_df = None
     want_main_area = False
     resizing_enabled = True
+    saved_gui_params = Setting(OrderedDict())
 
     def __init__(self):
         super().__init__()
@@ -42,7 +44,9 @@ class OWSparkFillNa(widget.OWWidget):
 
         # Create parameters Box.
         self.gui_parameters = OrderedDict()
-        self.gui_parameters['value'] = GuiParam(parent_widget = self.box, label = 'value', default_value = '0')
+        default_value = self.saved_gui_params.get('value', '0')
+        self.gui_parameters['value'] = GuiParam(parent_widget = self.box, label = 'value', default_value = default_value)
+        default_value = self.saved_gui_params.get('subset', 'None')
         self.gui_parameters['subset'] = GuiParam(parent_widget = self.box, label = 'fraction', default_value = 'None')
 
         self.action_box = gui.widgetBox(self.box)
@@ -57,3 +61,9 @@ class OWSparkFillNa(widget.OWWidget):
             value = self.gui_parameters['value'].get_usable_value()
             subset = self.gui_parameters['subset'].get_usable_value()
             self.send("DataFrame", self.in_df.fill(value, subset))
+            self.update_saved_gui_parameters()
+            self.hide()
+
+    def update_saved_gui_parameters(self):
+        for k in self.gui_parameters:
+            self.saved_gui_params[k] = self.gui_parameters[k].get_value()
